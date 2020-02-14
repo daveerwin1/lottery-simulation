cd5a2eb13f82d21d5201f15df33951a59c9c8bd1
Dave Erwin <derwin@concentricsky.com>
2020-02-13 21:34:15 -0800

    Added: service ofr random numbers

--- src/app/services/raffle.service.ts
+++ src/app/services/raffle.service.ts
@@ -5,5 +5,39 @@ import { Injectable } from '@angular/core';
 })
 export class RaffleService {
 
+  numberList: number[];
+
   constructor() { }
+
+  get winningNumber(): number {
+    return this.getRandomInt(250001);
+  }
+
+  get secondPrizeNumbers(): number[] {
+    return this.getRandomIntList(250001, 300);
+  }
+
+  get thirdPrizeNumbers(): number[] {
+    return this.getRandomIntList(250001, 1500);
+  }
+
+  private getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  private getRandomIntList(max, count) {
+
+    this.numberList = [];
+
+    for (let x = 0; x < count;) {
+      let number = this.getRandomInt(max);
+
+      if (!this.numberList.includes(number)) {
+        this.numberList.push(number);
+        count++;
+      }
+    }
+
+    return this.numberList;
+  }
 }
